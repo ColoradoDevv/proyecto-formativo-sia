28b479d08f60b2869e250e6239b52a29b491cfe9
ColoradoDevv <74389451+ColoradoDevv@users.noreply.github.com>
2026-03-15 22:34:22 -0500

    feat: Se corrigio el archivo serializer

--- backend/apps/users/__init__.py
+++
@@ -1,8 +0,0 @@
-from reset_framework_mongoengine import serializers
-from .models import User
-
-class UserSerializer(serializers.DocumentSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'created_at']
-        extra_kwargs = {'password': {'write_only': True}}
