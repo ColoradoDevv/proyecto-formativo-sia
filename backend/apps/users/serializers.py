545a366cd10b725371e576903dab7c165577b64e
ColoradoDevv <74389451+ColoradoDevv@users.noreply.github.com>
2026-03-15 22:57:38 -0500

    fix: Se corrigieron erroes en el serializer y model

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,8 +1,20 @@
-from rest_framework_mongoengine import serializers
+from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.DocumentSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password', 'created_at']
-        extra_kwargs = {'password': {'write_only': True}}
+class UserSerializer(serializers.Serializer):
+    id         = serializers.CharField(read_only=True)
+    username   = serializers.CharField(max_length=150)
+    email      = serializers.EmailField()
+    first_name = serializers.CharField(max_length=100, required=False, default='')
+    last_name  = serializers.CharField(max_length=100, required=False, default='')
+    password   = serializers.CharField(write_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return User(**validated_data).save()
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
