c8a90a16b7c33c2726c82bd9670ba6f1d6462773
D4nxw <dm510520@gmail.com>
2026-03-17 23:37:22 -0500

    feature(users) add the models of users, type_document, role

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,20 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(max_length=245)
+
+    def __str__(self):
+        return self.name
+
+class TypeDocument(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(max_length=245)
+
 
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    type_document = models.ForeignKey(Type_document,on_delete=models.RESTRICT)
-    number_document = models.IntegerField(unique=True,max_length=16)
+    type_document = models.ForeignKey(TypeDocument,on_delete=models.RESTRICT)
+    number_document = models.IntegerField(unique=True)
     role = models.ForeignKey(Role, on_delete=models.RESTRICT)
     date_start = models.DateField()
     date_end = models.DateField()
     email = models.EmailField(unique=True,max_length=254)
-    phone_number = models.IntegerField(unique=True,max_length=16)
+    phone_number = models.IntegerField(unique=True)
     direction = models.CharField(max_length=100)
     status = models.BooleanField(default=True)
-    second_phonenumber = models.IntegerField(null=True,max_length=16)
+    second_phonenumber = models.IntegerField(null=True)
     institutional_email = models.EmailField(null=True,unique=True,max_length=254)
     profile_picture = models.ImageField(null=True)
 
---
+++ backend/users/urls.py
@@ -0,0 +1,5 @@
+from . import views
+from django.urls import path
+urlpatterns = [
+    path('create/',views.create_user)
+]
