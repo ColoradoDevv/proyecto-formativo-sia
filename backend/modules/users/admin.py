87238cd6153eda84a16c17c1595d0e9b8a14bb37
ColoradoAlt <coloradodevv@gmail.com>
2026-03-18 01:03:31 -0500

    feat(admin): register User, Role, Type_document, Brand, Category and Material models in admin site

--- backend/modules/users/admin.py
+++ backend/modules/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Role, Type_document
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(Type_document)
