bdbc1b2722e2a59fc2c975a76fc31d6064f97940
ColoradoAlt <coloradodevv@gmail.com>
2026-03-18 00:58:07 -0500

    feat(products): add products module with Brand, Category, Consumable and Returnable material models

--- backend/modules/users/models.py
+++ backend/modules/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Type_document(models.Model):
     # agrega los campos que necesites
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=225)
+    description = models.CharField(max_length=225, null=True)
 
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class Type_document(models.Model):
 class Role(models.Model):
     # agrega los campos que necesites
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=225)
+    description = models.CharField(max_length=225, null=True)
 
     def __str__(self):
         return self.name
