3b36824c7dd625bae917e7ec157850b69ce263d3
Cristian Salazar <cristianssalaz@gmail.com>
2026-06-17 14:09:45 -0500

    feat: update HomePage layout and adjust Python version requirements

--- frontend/src/features/home/pages/HomePage.jsx
+++ frontend/src/features/home/pages/HomePage.jsx
@@ -1,18 +1,10 @@
-import Navbar  from "../../../shared/layouts/Navbar"
-import Sidebar  from "../../../shared/layouts/Sidebar"
+import HomeLayout from "../layouts/HomeLayout";
 
 export default function HomePage(){
     return (
-        <div className="h-screen flex flex-col">
-            <Navbar />
-
-            <div className="flex flex-1">
-                <Sidebar />
-
-                <main className="flex-1 w p-6 bg-background text-text-primary">
-                    <h2 className="text-h3">Hola!, Administrador.</h2>
-                    <p className="text-sm text-text-muted">Bienvenido al Sistema de Gestion Inventario.</p>
-                </main>
+        <div className="p-6">
+            <h2 className="text-h3">Hola!, Administrador.</h2>
+            <p className="text-sm text-text-muted">Bienvenido al Sistema de Gestion Inventario.</p>
 
             <div className="pt-4">
                 <HomeLayout/>
